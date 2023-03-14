1cfe9c47bb13bb3863f0aff7cb31ac728712ba93
nUestra <117568316+thatkidnUestra@users.noreply.github.com>
2023-03-14 18:01:08 +0300

    Initial commit

---
+++ handlers/users/start.py
@@ -0,0 +1,10 @@
+from aiogram import types
+from aiogram.dispatcher.filters import CommandStart
+
+from loader import dp
+from utils.db_api.db_commands import select_client, create_client
+
+
+@dp.message_handler(CommandStart())
+async def bot_start_no_state(message: types.Message):
+    await message.answer('Привет!')
