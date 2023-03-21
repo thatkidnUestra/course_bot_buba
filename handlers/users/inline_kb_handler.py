924060ee14d008af9e310ca66ac49c02ee8ddd33
bichief <sashamarx67@gmail.com>
2023-03-21 18:00:52 +0300

    Написание инлайн клавиатур

--- handlers/users/inline_kb_handler.py
+++ handlers/users/inline_kb_handler.py
@@ -20,10 +20,18 @@ async def go_back(call: types.CallbackQuery):
 async def get_random_number(call: types.CallbackQuery):
     number = random.randint(0, 10000)
 
-
     """
         await call.answer('хеех', show_alert=True) <- Это уведомление на весь экран
         await call.message.answer(f'Вот ваше число: {number}') <- Это для ОТПРАВКИ сообщения
     """
 
     await call.message.edit_text(f'Вот ваше число: {number}', reply_markup=inline_back)
+
+
+'''
+await call.message.delete()
+await call.message.answer_photo(
+photo=ссылка на фото,
+caption='Вот этот мем))))'
+)
+'''
