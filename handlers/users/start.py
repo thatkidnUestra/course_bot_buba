d0c70777462d72ef1b80f69bd0a36ed1da74ab1a
Alex <sashamarx67@gmail.com>
2023-03-25 21:04:10 +0300

    изменил текст кнопки назад + обработчик кнопки "назад в меню"

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,5 +1,5 @@
 from aiogram import types
-from aiogram.dispatcher.filters import CommandStart
+from aiogram.dispatcher.filters import CommandStart, Text
 
 from keyboards.default.menu_keyboard import menu
 from loader import dp
@@ -10,3 +10,11 @@ async def bot_start_no_state(message: types.Message):
     await message.answer(f'👋  Привет, {message.from_user.first_name}!\n'
                          f'🤖 Я бот, который поможет найти тебе тиммейта или команду\n\n'
                          f'<i> 👇Для взаимодействия со мной, пользуйся клавиатурой ниже</i>', reply_markup=menu)
+
+
+@dp.callback_query_handler(Text(equals='go_menu'))
+async def go_menu(call: types.CallbackQuery):
+    await call.message.delete()
+    await call.message.answer(f'👋  Привет, {call.from_user.first_name}!\n'
+                              f'🤖 Я бот, который поможет найти тебе тиммейта или команду\n\n'
+                              f'<i> 👇Для взаимодействия со мной, пользуйся клавиатурой ниже</i>', reply_markup=menu)
