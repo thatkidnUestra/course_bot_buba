8ee157be42829c1b297049c6235198bb309759da
Alex <sashamarx67@gmail.com>
2023-03-25 20:23:03 +0300

    начало проекта

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,7 +1,7 @@
 from .start import dp
-from .api_info import dp  # Зарегистрировали обработчик
-from .default_kb_handler import dp
-from .inline_kb_handler import dp
-from .get_file_id import dp
+# from .api_info import dp  # Зарегистрировали обработчик
+# from .default_kb_handler import dp
+# from .inline_kb_handler import dp
+# from .get_file_id import dp
 
 __all__ = ["dp"]
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,6 +4,9 @@ from aiogram.dispatcher.filters import CommandStart
 from keyboards.default.menu_keyboard import menu
 from loader import dp
 
+
 @dp.message_handler(CommandStart())
 async def bot_start_no_state(message: types.Message):
-    await message.answer('привет!', reply_markup=menu)
+    await message.answer(f'👋  Привет, {message.from_user.first_name}!\n'
+                         f'🤖 Я бот, который поможет найти тебе тиммейта или команду\n\n'
+                         f'<i> 👇Для взаимодействия со мной, пользуйся клавиатурой ниже</i>', reply_markup=menu)
