b3ffb76ebd8e1c928b5f88f7cca00405537020d6
thatkidnUestra <nuestra13@mail.ru>
2023-05-19 20:15:41 +0300

    upd start

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,14 +9,14 @@ from utils.db_api.db_commands import create_user
 @dp.message_handler(CommandStart())
 async def bot_start_no_state(message: types.Message):
     await create_user(telegram_id=message.from_user.id)
-    await message.answer(f'👋 Тебя приветствует Марти! {message.from_user.first_name}!\n'
+    await message.answer(f'👋 Тебя приветствует Марти!\n'
                          f'🤖 С помощью меня ты сможешь найти себе команду для своей любимой игры\n\n'
-                         f'<i> 👇Для взаимодействия со мной, пользуйся клавиатурой ниже</i>', reply_markup=menu)
+                         f'<i>👇Для взаимодействия со мной, пользуйся клавиатурой ниже</i>', reply_markup=menu)
 
 
 @dp.callback_query_handler(Text(equals='go_menu'))
 async def go_menu(call: types.CallbackQuery):
     await call.message.delete()
-    await call.message.answer(f'👋 Тебя приветствует Марти! {call.from_user.first_name}!\n'
+    await call.message.answer(f'👋 Тебя приветствует Марти!\n'
                               f'🤖 С помощью меня ты сможешь найти себе команду для своей любимой игры\n\n'
-                              f'<i> 👇Для взаимодействия со мной, пользуйся клавиатурой ниже</i>', reply_markup=menu)
+                              f'<i>👇Для взаимодействия со мной, пользуйся клавиатурой ниже</i>', reply_markup=menu)
