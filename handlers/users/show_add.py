2ef45143fd055fc9076deafb17ae662716ff3220
bichief <sashamarx67@gmail.com>
2023-05-24 19:22:57 +0300

    update pagination

--- handlers/users/show_add.py
+++ handlers/users/show_add.py
@@ -32,3 +32,32 @@ async def show_add_kb(message: types.Message):
                          )
 
 
+@dp.callback_query_handler(Text(startswith='go_next_'))
+async def go_next(call: types.CallbackQuery):
+    list_id = int(call.data.split('_')[2])
+    print(list_id)
+
+    data = await get_all_adds()
+    print(len(data))
+
+    if list_id >= len(data):
+        await call.answer('Анкеты закончились, подбор начался сначала.', show_alert=True)
+        list_id = 0
+
+    owner_id = data[list_id].owner
+    name = data[list_id].name
+    nickname = data[list_id].nickname
+    age = data[list_id].age
+    description = data[list_id].description
+    game = data[list_id].game
+
+    keyboard = await pagination_kb(
+        list_id=list_id,
+        telegram_id=owner_id
+    )
+
+    await call.message.edit_text('Смотри, что я для тебя нашел:\n\n'
+                                 f'{name} ({nickname}), {age} лет, {game}\n\n'
+                                 f'{description}\n\n',
+                                 reply_markup=keyboard
+                                 )
